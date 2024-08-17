80ad2a0e8fc1c33c97636d1b9a273ea5ebf13bb0
Matans94 <shomermatan@gmail.com>
2024-08-17 19:14:27 +0300

    first assay working

--- nav-btn.js
+++ nav-btn.js
@@ -1,17 +1,29 @@
-document.querySelector('.nav-button').addEventListener('click', function() {
-    const slidingWindow = document.getElementById('sliding-window');
-    if (slidingWindow.classList.contains('visible')) {
-        slidingWindow.classList.remove('visible');
+document.querySelector('#nav-button-div').addEventListener('click', function() {
+    const slidingWindow = document.getElementById('nav-button-div');
+    const menuIcon = document.getElementById("nav-button");
+    const menuItems = document.getElementById("menu-div");
+
+    if (slidingWindow.classList.contains('selected')) {
+        slidingWindow.classList.remove('selected');
+        menuIcon.classList.remove('selected');
+        menuItems.classList.remove('selected');
     } else {
-        slidingWindow.classList.add('visible');
+        slidingWindow.classList.add('selected');
+        menuIcon.classList.add('selected');
+        menuItems.classList.add('selected');
     }
 });
 
 document.addEventListener('click', function(event) {
-    const slidingWindow = document.getElementById('sliding-window');
-    const navButton = document.querySelector('.nav-button');
+    const slidingWindow = document.getElementById('nav-button-div');
+    const navButton = document.querySelector('#nav-button-div');
+    const menuIcon = document.getElementById("nav-button");
+    const menuItems = document.getElementById("menu-div");
 
     if (!slidingWindow.contains(event.target) && !navButton.contains(event.target)) {
-        slidingWindow.classList.remove('visible');
+        slidingWindow.classList.remove('selected');
+        menuIcon.classList.remove('selected');
+        menuItems.classList.remove('selected');
     }
 });
+
